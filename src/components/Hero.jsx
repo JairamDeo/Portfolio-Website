0e16a574e44ec26d4379e4a2fc700cced0bb15e0
Jairam Deo <jairamdeo2002@gmail.com>
2025-05-18 14:50:28 +0530

    Pdf updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import React, { Suspense } from "react";
-import {Link} from "react-router-dom";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Typewriter } from "react-simple-typewriter";
@@ -46,13 +45,13 @@ const Hero = () => {
             delaySpeed={1000}
           />
         </p>
-        <Link
-          to="/Jairam Deo_Full Stack Developer_Resume.pdf"
+        <a
+          href="/Jairam Deo_Full Stack Developer_Resume.pdf"
           download
           className="mt-6 inline-block bg-gradient-to-r from-purple-800 to-blue-900 text-white px-6 py-3 rounded-md hover:from-purple-700 hover:to-blue-800 transition duration-300 ease-in-out transform hover:scale-105"
         >
           Download CV
-        </Link>
+        </a>
 
       </motion.div>
     </section>
