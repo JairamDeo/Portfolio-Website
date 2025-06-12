72eff93712377007bc2284302e672ac71641ed69
Jairam Deo <jairamdeo2002@gmail.com>
2025-06-12 15:14:25 +0530

    aplied lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Education from "./components/Education";
+import React, { Suspense, lazy } from "react";
+
+// Lazy-loaded components
+const Navbar = lazy(() => import("./components/Navbar"));
+const Hero = lazy(() => import("./components/Hero"));
+const About = lazy(() => import("./components/About"));
+const Skills = lazy(() => import("./components/Skills"));
+const Experience = lazy(() => import("./components/Experience"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
+const Education = lazy(() => import("./components/Education"));
 
 const App = () => {
-
-
   return (
     <div className="bg-gray-900 text-white">
-      <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Education/>
-      <Contact />
-      <Footer />
+      <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+        <Navbar />
+        <Hero />
+        <About />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Education />
+        <Contact />
+        <Footer />
+      </Suspense>
     </div>
   );
 };
